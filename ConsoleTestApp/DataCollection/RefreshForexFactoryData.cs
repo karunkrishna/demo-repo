15dc05af2e1b7daaa4043c4e5b18f5179e398318
Karunyan Krishnasamy <karun.krishna@gmail.com>
2016-08-05 15:59:30 -0400

    Updated program to load FF data in realtime and save to database. Currenly it is a local version, with hardcoded id and datetime. I would like to expand it to realtime data and error handle Internet issue, as well as add tool tip.

---
+++ ConsoleTestApp/DataCollection/RefreshForexFactoryData.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using HtmlAgilityPack;
+using MySql.Data.MySqlClient;
+
+namespace ConsoleTestApp.DataCollection
+{
+    class RefreshForexFactoryData
+    {
+        private int id;
+        private DateTime eventTime = new DateTime();
+
+        private Timer executeFetchEvent = new Timer();
+        private Timer LoopTillCompleted = new Timer();
+        private MySqlConnection dbConn = new MySqlConnection("server = localhost; database=algo;uid=root;password=Password1;");
+
+        public RefreshForexFactoryData(int ID, DateTime EventTime)
+        {
+            // This class responsible for monitoring and updating the database value in realtime on a per event basis. 
+            // TO DO: Currently the id and date is hard code. But in Time I hope to update to behave in realtime. 
+            // TO DO: Tooltip and also error handling when internet connection is diconnected. 
+
+            this.id = ID;
+            this.eventTime = EventTime;
+
+            //TO DO: Use current system time
+            //DateTime currentTime = DateTime.Now;
+
+            DateTime currentTime = new DateTime(2016, 06, 22, 9, 59, 40);
+
+            //TO DO error handle when time < 0
+            TimeSpan timeToEvent = eventTime - currentTime;
+
+            executeFetchEvent.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            executeFetchEvent.Interval = (timeToEvent.TotalMilliseconds > 0) ? timeToEvent.TotalMilliseconds : 10;
+            executeFetchEvent.Enabled = true;
+        }
+
+
+        private void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            // TO DO: Stop loop if Internet is disconnected or if Loop exceeds 10 minutes. 
+            Console.WriteLine(string.Format("Begin fetching update for {0}  @  {1}",id,eventTime));
+
+            executeFetchEvent.Enabled = false;
+            LoopTillCompleted.Interval = 5000;
+            LoopTillCompleted.Enabled = true;
+            LoopTillCompleted.Elapsed += new ElapsedEventHandler(OnRefreshEvent);
+
+
+        }
+
+        private void ParseLocalHttpData(int eventId)
+        {
+            string refreshedActual = "";
+            string url = @"C:/Users/Karunyan/Documents/Reports/testLocalHTML/doing.html";
+
+
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            HtmlWeb hw = new HtmlWeb();
+            doc = hw.Load(url);
+
+            HtmlNodeCollection actualNode = doc.DocumentNode.SelectNodes(string.Format("//*[@id='flexBox_flex_calendar_mainCal']//*[contains(@data-eventid,{0})]//td[contains(@class,'actual')]", eventId));
+            HtmlNodeCollection previousNode = doc.DocumentNode.SelectNodes(string.Format("//*[@id='flexBox_flex_calendar_mainCal']//*[contains(@data-eventid,{0})]//td[contains(@class,'previous')]", eventId));
+            HtmlNodeCollection forecastNode = doc.DocumentNode.SelectNodes(string.Format("//*[@id='flexBox_flex_calendar_mainCal']//*[contains(@data-eventid,{0})]//td[contains(@class,'forecast')]", eventId));
+
+            if (previousNode[0].InnerText != "" || forecastNode[0].InnerText != "")
+            {
+                if (actualNode[0].InnerHtml.Contains("Request"))
+                {
+                    Console.WriteLine(string.Format("{0} Request icon still present, continue fetching...",id));
+                    //aTimer.Enabled = true;
+                }
+                else
+                {
+                    refreshedActual = actualNode[0].InnerText;
+                    Console.WriteLine(string.Format("{0} Actual: {1}",id,refreshedActual));
+                    LoopTillCompleted.Enabled = false;
+                    SaveDataToDatabase(id, refreshedActual);
+                }
+            }
+        }
+
+        private void OnRefreshEvent(object source, ElapsedEventArgs e)
+        {
+            this.ParseLocalHttpData(id);
+        }
+
+        private void SaveDataToDatabase(int id, string actual)
+        {
+            string updateActualData = string.Format("update economiceventdataRefresh set actual = '{0}'where id = '{1}'", actual, id);
+
+            //http://stackoverflow.com/questions/6556058/do-i-have-to-lock-the-database-connections-when-multithreading
+            dbConn.Open();
+            MySqlCommand cmd = new MySqlCommand(updateActualData, dbConn);
+            cmd.ExecuteNonQuery();
+            dbConn.Close();
+        }
+
+    }
+}
--- ConsoleTestApp/Program.cs
+++ ConsoleTestApp/Program.cs
@@ -5,56 +5,150 @@ using System.Text;
 using System.Threading.Tasks;
 using ConsoleTestApp.DataCollection;
 using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System.Globalization;
+//using System.Threading;
+//using UITimer = System.Timers.Timer;
+using System.Timers;
+    //using System.Timers.ElapsedEventHandler;
 
 namespace ConsoleTestApp
 {
     class Program
     {
+        //private List<int> currentDayEventId = new List<int>();  
+         
+        private static MySqlConnection dbConn = new MySqlConnection("server = localhost; database=algo;uid=root;password=Password1;");
+        private Timer executeFetchEvent = new Timer();
+
+
         static void Main(string[] args)
         {
+            Dictionary<int, DateTime> currentEventandTime = new Dictionary<int, DateTime>();
+            Dictionary<int, string> isIssueCurrentEventandTime = new Dictionary<int, string>();
+            string queryEventId = "select id, concat(concat(datenum,' '),timeEst) from ( select * from economiceventdataRefresh where forecast <> '' or previous <> '') xd where xd.datenum = '2016-06-22'";
 
-            //ForexFactory csvForexFactory = new ForexFactory();
-            //csvForexFactory.FetchHTTPData();
+            dbConn.Open();
+            using (MySqlCommand command = new MySqlCommand(queryEventId, dbConn))
+            {
+                using (MySqlDataReader reader = command.ExecuteReader())
+                {
 
+                    while (reader.Read())
+                    {
+                        DateTime convertedDate = new DateTime();
 
-            //CommitmentOfTraders csv = new CommitmentOfTraders();
-            //StateStreet csvStateStreet = new StateStreet();
-            LocalForexFactory csvLocalForexFactory = new LocalForexFactory();
-            csvLocalForexFactory.FetchLocalHTTPData();
+                        if (DateTime.TryParse(reader.GetString(1), out convertedDate))
+                        {
+                            currentEventandTime.Add(reader.GetInt32(0), convertedDate);
+                        }
+                        else
+                        {
+                            //Test out this case please....
+                            isIssueCurrentEventandTime.Add(reader.GetInt32(0),reader.GetString(1));
+                        }
+                    }
+                }
+            }
+            dbConn.Close();
 
+            //createTimerEvent();
+            DateTime eventTime = new DateTime(2016, 06, 22, 10, 00, 00);
 
+            //http://stackoverflow.com/questions/314008/programatically-using-a-string-as-object-name-when-instantiating-an-object
 
-            //YahooFinance checkUrl = new YahooFinance();
-            //EconomicEventHeatmap getHeatmap = new EconomicEventHeatmap();
+            foreach (KeyValuePair<int, DateTime> id in currentEventandTime)
+            {
 
-            //EMailAlert sendEmail = new EMailAlert();
+                new RefreshForexFactoryData(id.Key, id.Value);
+            }
 
-            //Morningstar getFinacials = new Morningstar();
-
-            //Zacks date = new Zacks();
-            //CMEGroup settlement = new CMEGroup();
-            // YahooFinance vix = new YahooFinance();
-            // vix.SaveSingleData("^VIX");
-
-            // KeyAlerts getData = new KeyAlerts();
-
-            //Load historical data
-            //csv.CsvFetchCommitmentofTraders();
-            //csvStateStreet.DownloadXLSFile();
-            //csvForexFactory.FetchHTTPData();
-
-            //Sunday secheduler to collect data
-            //csv.HttpFetchCommitmentOfTraders();
-
-            //Time sensitive scheduler to collect data
-            //
-
-            //Download Equitities data
-            //checkUrl.RunURLChecker();
-
-
+            //RefreshForexFactoryData eventA = new RefreshForexFactoryData(60816, eventTime);
+            //RefreshForexFactoryData eventB = new RefreshForexFactoryData(62333, eventTime);
 
             Console.ReadLine();
         }
+
+        #region Test cases for various HTML Parsing Programs
+
+        // System.Collections.Generic.KeyValuePair<int, System.DateTime>
+
+
+        //LocalForexFactory csvLocalForexFactory = new LocalForexFactory();
+        //csvLocalForexFactory.FetchLocalHTTPData();
+
+        //ForexFactory csvForexFactory = new ForexFactory();
+        //csvForexFactory.FetchHTTPData();
+
+
+        //CommitmentOfTraders csv = new CommitmentOfTraders();
+        //StateStreet csvStateStreet = new StateStreet();
+
+
+
+
+        //YahooFinance checkUrl = new YahooFinance();
+        //EconomicEventHeatmap getHeatmap = new EconomicEventHeatmap();
+
+        //EMailAlert sendEmail = new EMailAlert();
+
+        //Morningstar getFinacials = new Morningstar();
+
+        //Zacks date = new Zacks();
+        //CMEGroup settlement = new CMEGroup();
+        // YahooFinance vix = new YahooFinance();
+        // vix.SaveSingleData("^VIX");
+
+        // KeyAlerts getData = new KeyAlerts();
+
+        //Load historical data
+        //csv.CsvFetchCommitmentofTraders();
+        //csvStateStreet.DownloadXLSFile();
+        //csvForexFactory.FetchHTTPData();
+
+        //Sunday secheduler to collect data
+        //csv.HttpFetchCommitmentOfTraders();
+
+        //Time sensitive scheduler to collect data
+        //
+
+        //Download Equitities data
+        //checkUrl.RunURLChecker();
+
+
+        #endregion
+
+        private void createTimerEvent()
+        {
+            // I WILL BE SETTING UP A SCHEDULE TASK BASED OF ELAPSED TIME HERE. 
+            // I HOPE THAT IT DOES NOT GET MESSY. LETS DO IT FOR ONE CASE, AND THEN EXPAND TO MULTIPLE THREADS. 
+
+            //hardcoded data: 
+            Dictionary<int, DateTime> testEventDetails = new Dictionary<int, DateTime>();
+            int testId = 60816;
+            DateTime testDateTime = new DateTime(2016, 06, 22, 10, 00, 00);
+            testEventDetails.Add(testId, testDateTime);
+
+            DateTime currentTime = DateTime.Now;
+            TimeSpan timeToEvent = testDateTime - currentTime;
+            double totalMsToEvent = timeToEvent.TotalMilliseconds;
+
+            //Now I have this elapsed time, I need a way to trigger the event, and actually do the call request to the HTML page. 
+            //Create a unix comand to quickly convert before and after page. 
+
+
+            executeFetchEvent.Interval = totalMsToEvent;
+            executeFetchEvent.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+
+        }
+        private  void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            Console.WriteLine("We have went about fetching the data");
+            //this.ParseLocalHttpData(@"C:/Users/Karunyan/Documents/Reports/testLocalHTML/doing.html", 60816);
+            executeFetchEvent.Enabled = false;
+        }
+
     }
+
 }
+
